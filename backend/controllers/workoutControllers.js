b9653f9b861187f392d7187be9c842fc1b66cb28
Abheelash-Mishra <abheelashmishra@gmail.com>
2023-10-24 19:08:19 +0530

    Form now shows empty fields error

--- backend/controllers/workoutControllers.js
+++ backend/controllers/workoutControllers.js
@@ -13,12 +13,12 @@ const getOneWorkout = async (req, res) => {
 	const { id } = req.params;
 
 	if (!mongoose.Types.ObjectId.isValid(id)) {
-		return res.status(404).json({ msg: "Invalid ID" });
+		return res.status(404).json({ error: "Invalid ID" });
 	}
 	const workout = await Workout.findById(id);
 
 	if (!workout) {
-		return res.status(404).json({ msg: `No workout with ID: ${ id }` });
+		return res.status(404).json({ error: `No workout with ID: ${ id }` });
 	}
 
 	res.status(200).json(workout);
@@ -27,11 +27,20 @@ const getOneWorkout = async (req, res) => {
 // POST one workout
 const createWorkout = async (req, res) => {
 	const { title, reps, load } = req.body;
+
+	let emptyFields = [];
+	if(!title) emptyFields.push("title");
+	if(!reps) emptyFields.push("reps");
+	if(!load) emptyFields.push("load");
+
+	if(emptyFields.length > 0)
+		return res.status(400).json({ error: "Please Fill In All The Fields!", emptyFields });
+
 	try {
 		const newWorkout = await Workout.create({ title, reps, load });
-		res.status(200).json(newWorkout);
-	} catch (err) {
-		res.status(400).json({ msg: err.message });
+		res.status(200).json(newWorkout, emptyFields);
+	} catch (error) {
+		res.status(400).json({ error: error.message });
 	}
 };
 
