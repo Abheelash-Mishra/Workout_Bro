4a41f208a8eca52c7cf7f110e1bf71aebcc333bb
Abheelash-Mishra <abheelashmishra@gmail.com>
2023-10-28 15:26:41 +0530

    Signing up and hashing passwords done

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,17 +1,36 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-	email:{
+	email: {
 		type: String,
 		required: true,
 		unique: true
 	},
-	password:{
+	password: {
 		type: String,
 		required: true,
 		minlength: 5
 	}
 });
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+
+User.signup = async function (email, password) {
+	const exists = await this.findOne({ email });
+
+	if (exists) {
+		throw new Error('User already exists');
+	}
+
+	const salt = await bcrypt.genSalt(10);
+	const hash = await bcrypt.hash(password, salt);
+
+	const user = await this.create({ email, password: hash });
+
+	return user;
+};
+
+module.exports = User;
