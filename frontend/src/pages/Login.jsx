4260a5037f4403e30a3ffe8345b34ad0ddd2bb45
Abheelash-Mishra <abheelashmishra@gmail.com>
2023-10-30 09:06:22 +0530

    fixup! Login and Signup Forms added

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -24,8 +24,8 @@ const Login = () => {
 			<label>Password: </label>
 			<input
 				type="password"
-				onChange={ (e) => setEmail(e.target.value) }
-				value={ email }
+				onChange={ (e) => setPassword(e.target.value) }
+				value={ password }
 			/>
 
 			<button>Login</button>
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -26,8 +26,8 @@ const Signup = () => {
 			<label>Password: </label>
 			<input
 				type="password"
-				onChange={ (e) => setEmail(e.target.value) }
-				value={ email }
+				onChange={ (e) => setPassword(e.target.value) }
+				value={ password }
 			/>
 
 			<button disabled={isLoading}>Sign Up</button>
