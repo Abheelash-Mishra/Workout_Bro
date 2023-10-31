b411e01781a528a9c775dbc1cf2985a13c4075a1
Abheelash-Mishra <abheelashmishra@gmail.com>
2023-10-31 09:38:09 +0530

    Protected react routes from users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext.js";
 import "./index.css";
 
 // Pages & Routes
@@ -11,15 +12,17 @@ import Signup from "./pages/Signup.jsx";
 
 
 function App() {
+	const { user } = useAuthContext();
+
 	return (
 		<div className="App">
 			<BrowserRouter>
-				<Navbar />
+				<Navbar/>
 				<div className="pages">
 					<Routes>
-						<Route path={"/"} element={<Home />} />
-						<Route path={"/login"} element={<Login />} />
-						<Route path={"/signup"} element={<Signup />} />
+						<Route path={ "/" } element={ user ? <Home/> : <Navigate to={"/login"} /> }/>
+						<Route path={ "/login" } element={ !user ? <Login/> : <Navigate to={"/"} /> }/>
+						<Route path={ "/signup" } element={ !user ? <Signup/> : <Navigate to={"/"} /> }/>
 					</Routes>
 				</div>
 			</BrowserRouter>
